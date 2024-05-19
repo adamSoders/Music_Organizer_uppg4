aba9c91f04f50689011fa69aa0a2b14fbc25c65d
Advhkil <167900802+Advhkil@users.noreply.github.com>
2024-05-20 01:03:50 +0300

    Update HTMLexporter.java

--- src/model/HTMLexporter.java
+++ src/model/HTMLexporter.java
@@ -1,22 +1,54 @@
 package model;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
 
 import controller.MusicOrganizerController;
 
 public class HTMLexporter implements ExportBehaviour{
 	
 	MusicOrganizerController controller;
+	BufferedWriter buf;
+	int extraSpace;
 	
-	public HTMLexporter(MusicOrganizerController controller) {
+	public HTMLexporter(File selectedFile, MusicOrganizerController controller) {
 		this.controller = controller;
 	}
 	
 
 	@Override
-	public void exportFile(Album root) { // Exports file as HTML 
-		// TODO Auto-generated method stub
-		
+	public void exportFile(File selectedFile,Album root) { // Exports file as HTML 
+		extraSpace = 0;
+		try {
+			buf = new BufferedWriter(new FileWriter(selectedFile));
+			subAlbumFormating(root);
+			buf.close();
+		}catch(IOException e){
+			
+		}
 	}
-
+	
+	public void subAlbumFormating(Album root) {
+		try {
+			if(extraSpace!=0) {
+				buf.write("|");
+				for(int i=0;i<extraSpace;i++) {
+					buf.write("-");
+				}
+			}
+			buf.write(root.toString()+": "+root.getTracks()+"<br />");
+		}catch(IOException e) {
+			
+		}
+		if(root.getSubAlbums()!=null) {
+			for(Map.Entry<String, Album> subAlbum : root.getSubAlbums().entrySet()) {
+				extraSpace++;
+				subAlbumFormating(subAlbum.getValue());
+			}
+		}
+		extraSpace--;
+	}
 }

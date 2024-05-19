956c01ca8afa4fc890f35b56d548b922d2cd1782
Advhkil <167900802+Advhkil@users.noreply.github.com>
2024-05-20 01:02:06 +0300

    Update ExportBehaviour.java

--- src/model/ExportBehaviour.java
+++ src/model/ExportBehaviour.java
@@ -4,5 +4,5 @@ import java.io.File;
 
 public interface ExportBehaviour { // Implemented by different classes to that export files in certain ways
 
-	public void exportFile(Album root);
+	public void exportFile(File selectedFile, Album root);
 }

29d109764ef387c7574d2cebdcd276c31ae50632
Advhkil <167900802+Advhkil@users.noreply.github.com>
2024-05-20 19:08:33 +0300

    Update HTMLexporter.java

--- src/model/HTMLexporter.java
+++ src/model/HTMLexporter.java
@@ -21,11 +21,11 @@ public class HTMLexporter implements ExportBehaviour{
 
 	@Override
 	public void exportFile(File selectedFile,Album root) { // Exports file as HTML 
-		extraSpace = 0;
+		extraSpace = 0; //Counts Depth from root file
 		try {
 			buf = new BufferedWriter(new FileWriter(selectedFile));
-			subAlbumFormating(root);
-			buf.close();
+			subAlbumFormating(root); //Start of writing loop
+			buf.close(); //Commits changes to file
 		}catch(IOException e){
 			
 		}
@@ -33,22 +33,22 @@ public class HTMLexporter implements ExportBehaviour{
 	
 	public void subAlbumFormating(Album root) {
 		try {
-			if(extraSpace!=0) {
+			if(extraSpace!=0) { //If not the true root album
 				buf.write("|");
-				for(int i=0;i<extraSpace;i++) {
+				for(int i=0;i<extraSpace;i++) { //Adds extra dashes depending on depth
 					buf.write("-");
 				}
 			}
-			buf.write(root.toString()+": "+root.getTracks()+"<br />");
+			buf.write(root.toString()+": "+root.getTracks()+"<br />"); //Gets name of album and what tracks it contains
 		}catch(IOException e) {
 			
 		}
 		if(root.getSubAlbums()!=null) {
-			for(Map.Entry<String, Album> subAlbum : root.getSubAlbums().entrySet()) {
-				extraSpace++;
-				subAlbumFormating(subAlbum.getValue());
+			for(Map.Entry<String, Album> subAlbum : root.getSubAlbums().entrySet()) { //For each album
+				extraSpace++; //incrases depth by one
+				subAlbumFormating(subAlbum.getValue()); //runs on sub albums
 			}
 		}
-		extraSpace--;
+		extraSpace--; //deccrases depth by one
 	}
 }
